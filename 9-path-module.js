eb94220ffb6a64e80ee4d7750c192b064304b98f
toppyc4 <topwstp@gmail.com>
2023-12-18 16:44:18 +0700

    first commit

---
+++ 9-path-module.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+console.log(path.sep);
+
+const filePath = path.join("/content", "subfolder", "text.txt");
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolte = path.resolve(__dirname, "content", "subfolder", "text.txt");
+console.log(absolte);
